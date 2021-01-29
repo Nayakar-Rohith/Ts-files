6ba0dca2d8bd110eb38a94af8b10b65e5a3658d6
Nayakar-Rohith <73525607+Nayakar-Rohith@users.noreply.github.com>
2021-01-29 12:11:30 +0530

    Add files via upload

---
+++ task.ts
@@ -0,0 +1,10 @@
+export default class Task{
+    _id: string;
+    title: string;
+    _listId: string;
+    completed: boolean;
+    notes:string;
+    progress:string;
+    deadline: string;
+    submittime: boolean;
+}
